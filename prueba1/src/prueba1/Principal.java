e2a8c1b10ce79abbf11a3a626227d1ddbad94881
“Your <you@example.com>
2024-03-05 14:04:26 +0100

    Segunda validación de la clase Principal

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -2,4 +2,9 @@ package prueba1;
 
 public class Principal {
 
+		public static void main(String[] args) {
+			System.out.println("Hola mundo java-git");
+			System.out.println("Segunda validacion");
+		}
+
 }
